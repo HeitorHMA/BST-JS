9e8a0be9aa43d84532573bbd3b3242d75b474265
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-09-02 11:20:15 -0300

    create function

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-//Create Node class//
-class node{
-    constructor(data){
-        this.data = data;
-        this.left = null;
-        this.right = null;
-    }
-};
-class BST{
-    
+//Binary Search Tree//
+class treeNode {
+    constructor (value){
+        this.value = value
+        this.left = null
+        this.right = null
+}}
+let arr = [1,2,3,4,5,6,7];
+let n = arr.length;
+function createBST(array , start , end){
+    let mid = parseInt((start + end) / 2);
+    let rootNode = new treeNode (array [mid])
+    rootNode.left = createBST (array,start,mid - 1)
+    rootNode.left = createBST (array,mid + 1,end)
+    return treeNode
 }
+console.log (createBST(arr , 0 , n - 1))
