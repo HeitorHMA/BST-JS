dd843b8e702d05d5c638bc275544eda703e106fb
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-09-09 11:56:35 -0300

    create Node class and Tree class

--- BST.js
+++ BST.js
@@ -1,1 +1,15 @@
-sss
+//Node Class//
+class Node{
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+//class Tree//
+class Tree {
+    constructor(root){
+        this.root = root;
+    }
+}
+//buildTree Function//
