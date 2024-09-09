7f3b37c3af493d76d024aa58de7ead83359cd190
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-09-09 13:20:53 -0300

    create new merge function

--- BST.js
+++ BST.js
@@ -1,4 +1,5 @@
 //Node Class//
+
 class Node{
     constructor(data){
         this.data = data;
@@ -7,41 +8,61 @@ class Node{
     }
 }
 //class Tree//
+
 class Tree {
     constructor(root){
         this.root = root;
     }
 }
 //filter array function//
+
 function filterDuplicates (array){
     return array.filter((value,index) => array.indexOf(value) === index);
 }
 //merge function//
-function merge (array1 , array2){
-    fI = 0;
-    sI =  0;
-    firstArr = array1
-    secondArr = array2
-    sortedArr = [];
-while (fI < firstArr.lenght && sI < secondArr.lenght) {
-    if (firstArr[fI] < secondArr[sI]){
-        sortedArr.push (firstArr[fI])
-        fI++
+
+function merge (left,right){
+    let result = [];
+    let i = 0;
+    let j = 0;
+    while (i < left.length && j < right.length){
+        if(left[i]<right[j]){
+            result.push(left[i])
+            i++
+        }
+        else{
+            result.push(right[j])
+            j++
+        }
     }
-    else{
-        sortedArr.push (secondArr[sI])
-        sI++
-    }}
-    return sortedArr
+    while (i < left.length){
+        result.push(left[i])
+        i++
+    }
+    while (j < right.length){
+        result.push(right[j])
+        j++
+    }
+    return result;
 }
 
+// sort Function//
+
+function sort (array){
+    if(array.length <= 1){
+    return array;}
+    let mid = Math.floor(array.length/2);
+    let left = array.slice(0,mid)
+    let right = array.slice(mid)
+    return merge (sort(left) , sort(right));
+}
 
 
 //buildTree Function//
 function buildTree(array){
     let treeArray = filterDuplicates (array);
     return treeArray
- }
-const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-const arr2 = [1, 8, 9, 23, 8, 9, 14, 3, 5, 7, 9, 67, 6345, 324]
-console.log(merge ( arr, arr2)) 
+ };
+const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const sarg = [14,3,5,2,8];
+console.log(sort(sarg))
